5ec0da19c6c7eba68882bb22b81b0cc30b679feb
hygor <hygor.henrique@live.com>
2022-10-31 20:49:09 -0300

    SEO Tags

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
+<!DOCTYPE html>
 <html>
 <meta charset="UTF-8">
+<meta name="description" content="MP Envelopamento de Veículos e Decor">
+<meta name="robots" content="Envelopamento, envelopamento, envelopamento de veiculos, envelopalemento de moveis, envelopamento de eletros">
+<meta name="googlebot" content="Envelopamento, envelopamento, envelopamento de veiculos, envelopalemento de moveis, envelopamento de eletros">
     <head>
         <title>MP Envelopamento</title>
         <link rel="stylesheet" type="text/css" href="assets\index.css">
