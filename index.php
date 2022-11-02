f14617818db591e6fb3fbd623134146ea1f6b623
hygor <hygor.henrique@live.com>
2022-11-02 12:24:13 -0300

    Mods

--- index.php
+++ index.php
@@ -7,12 +7,29 @@
     <head>
         <title>MP Envelopamento</title>
         <link rel="stylesheet" type="text/css" href="assets\index.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
     </head>
 <body>
     <div class="bar">
         <div class="logo">
             <img src="assets\img\logo.png" alt="MP Envelopamento" width="150">
         </div>
+
+        <nav class="navigation">
+            <a href="#incio">Início</a>
+            <a href="#sobrenos">Sobre Nós</a>
+            <a href="#servicos">Serviços</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#contato">Contato</a>
+        </nav>
     </div><!--bar-->
+
+    <div class="inicio">
+            <video src="assets\vid\mpvidinst.mp4" loop muted autoplay></video>
+            <div class="overlay"></div>
+            <h1>MP Envelopamento</h1>
+    </div><!--inicio-->
 </body>
 </html>
