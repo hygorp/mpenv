6875bc5d885ca917204be8befaf85bd0035e50a6
Hygor Henrique <hygor.henrique@live.com>
2022-11-08 13:39:38 -0300

    Finalizado¹

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
             <a href="#incio">Início</a>
             <a href="#servicos">Serviços</a>
             <a href="#portfolio">Portfolio</a>
-            <a href="#contato">Contato</a>
+            <a href="#footer">Contato</a>
         </nav>
     </div><!--bar-->
 
@@ -29,10 +29,11 @@
             <video src="assets\vid\mpvidinst.mp4" loop muted autoplay></video>
             <div class="overlay"></div>
             <div class="apresentacao">
-                <h1>A MP Envelopamento</h1>
-                <p>
-                    A Mp Envelopamento faz o envelopamento ou adesivação de veículos, paredes, móveis, entre outros, usando materiais de alta qualidade e perfomance. Nossos trabalhos são executados com maestria e garantimos um resultado incrível. Trabalhamos com seriedade, buscando sempre a excelência. Sua satisfação é nossa prioridade. Somos a principal escolha em Maceió, então não perca tempo!
-                </p>
+                <div class="whatsapp">
+                    <a href="https://wa.me/558299937569"><img src="https://img.icons8.com/color/512/whatsapp.png" width="128"></a><br>
+                    <span>Solicite um Orçamento</span>
+                </div><!--whatsapp-->
+                <iframe width="460" height="315" src="https://www.youtube.com/embed/_yhXdRIq-N0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
     </div><!--inicio-->
     <div class="servicos" id="portfolio">
@@ -46,7 +47,7 @@
         <div class="section"><img src="assets\img\port05.jpeg"><br>Imagem 05</div>
         <div class="section"><img src="assets\img\port6.jpeg"><br>Imagem 06</div>
     </div>
-    <div class="footer">
+    <div class="footer" id="footer">
         <div class="map">
             <p>Nossa Localização</p>
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15733.402179406101!2d-35.7095372!3d-9.6510587!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x277dfa273189b542!2sMP%20Envelopamento%2FAdesiva%C3%A7%C3%A3o%20(Automotivo%20e%20decor)!5e0!3m2!1spt-BR!2sbr!4v1667911086364!5m2!1spt-BR!2sbr" width="400" height="210" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
@@ -62,15 +63,6 @@
                 <a href="https://wa.me/558299937569"><img src="https://img.icons8.com/color/512/whatsapp.png" width="32"></a>
             </div><!--social-->
         </div><!--funcionamento-->
-
-        <div class="frm-contato">
-                <p>Deixe uma Mensagem!</p>
-                <form>
-                    <input type="text" name="nome" placeholder="Nome">
-                    <input type="text" name="telefone" placeholder="Telefone">
-                    <textarea name="mensagem" placeholder="Digite sua mensagem..."></textarea>
-                </form>
-            </div><!--frm-contato--> 
     </div><!--footer-->
 </body>
 </html>
